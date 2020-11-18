d066a39bf22cc5e762e3b9ae40976f8125c2f4af
Nithin <nithin.sagar3@gmail.com>
2020-11-18 21:00:54 +0530

    The starting of fire

---
+++ app.js
@@ -0,0 +1,49 @@
+//dot env config
+require('dotenv').config();
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+//Bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//method-override
+app.use(methodOverride('_method'));
+
+// parse application/json
+app.use(bodyParser.json());
+
+//Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//Connect flash
+app.use(flash());
+
+//Global variables
+app.use((req, res, next) => {
+    
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+app.use('/', require('./routes/index'));
+
+const server = app.listen(PORT, console.log(`Server started on port ${PORT}`));
