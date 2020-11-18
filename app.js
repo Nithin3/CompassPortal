b8d0ae2a0aa0894d2081a9a893f36c80e3cb5f9c
Nithin <nithin.sagar3@gmail.com>
2020-11-18 22:00:57 +0530

    Worked on nav bar and dashboard

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 const flash = require('connect-flash');
@@ -12,9 +13,10 @@ const PORT = process.env.PORT || 3000;
 
 //EJS
 app.use(expressLayouts);
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static("public"));
 
 //Bodyparser
 app.use(bodyParser.urlencoded({ extended: false }));
