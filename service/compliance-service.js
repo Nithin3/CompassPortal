8bb023f8d700af2ce4015dbed9b1208c163ae0b9
Nithin <nithin.sagar3@gmail.com>
2020-12-08 23:52:48 +0530

    getPatientComplianceData

--- service/compliance-service.js
+++ service/compliance-service.js
@@ -2,6 +2,7 @@
 'use strict';
 
 //import modules
+const axios = require('axios');
 const moment = require('moment');
 const viewDateTimeFormat = "MM/DD/YYYY hh:mm a";
 const chartDateFormat = "MM/DD/YYYY";
@@ -19,3 +20,18 @@ const PENDING = "pending";
 const IN_PROGRESS = "in progress";
 const COMPLETED = "completed";
 const EXPIRED = "expired";
+
+function getPatientComplianceData(patientPin){
+    var arr = []
+    axios.get('http://localhost:8080/CompassAPI/rest/activityinstances?patientPin='+patientPin)
+    .then(response => {
+        arr = (response.data._embedded.activityInstances);
+        arr.sort(function(a, b) {
+            return (a.activityInstance.startTime < b.activityInstance.startTime) ? -1 : ((a.activityInstance.startTime > b.activityInstance.startTime) ? 1 : 0);
+        });
+        return arr;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
